ff854b7ad3ee35831eb2ed0678567371b3147ff3
Alex Soll <alexsell72@gmail.com>
2024-06-09 20:03:34 +0300

    update

--- index.js
+++ index.js
@@ -36,8 +36,10 @@ bot.on('message', async (msg) => {
     }
 
     if(msg?.web_app_data?.data){
+        console.log('send')
         try {
             const data = JSON.parse(msg?.web_app_data?.data)
+            console.log(data)
 
             await bot.sendMessage(chatId, 'Спасибо за обратную связь')
             await bot.sendMessage(chatId, `Ваша страна: ${data?.country}`)
