4dcee355342f2cb908f195509c99b685140f1c80
Alex Soll <alexsell72@gmail.com>
2024-06-09 16:19:00 +0300

    inicial

---
+++ index.js
@@ -0,0 +1,31 @@
+
+const TelegramBot = require('node-telegram-bot-api');
+
+// replace the value below with the Telegram token you receive from @BotFather
+const token = '7470147033:AAGFtrD7AoJbS9-PRYGhAANIHhPAgU6CYzg';
+
+webAppUrl = 'https://ya.ru'
+
+// Create a bot that uses 'polling' to fetch new updates
+const bot = new TelegramBot(token, {polling: true});
+
+// Listen for any kind of message. There are different kinds of
+// messages.
+bot.on('message', async (msg) => {
+    const chatId = msg.chat.id;
+
+    const text = msg.text
+
+    if(text === '/start'){
+        // send a message to the chat acknowledging receipt of their message
+        await bot.sendMessage(chatId, 'Ниже появится кнопка заполни форму', {
+            reply_markup: {
+                inline_keyboard: [
+                    [{text: 'Сделать заказ', web_app: {url: webAppUrl}}]
+                ]
+            }
+        });
+    }
+
+
+});
