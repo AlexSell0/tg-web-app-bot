2e56e315fcea24f2408c23316daf25a2d6c1a5f1
Alex Soll <alexsell72@gmail.com>
2024-08-23 17:05:38 +0300

    add header size

--- src/components/ui/header/Header.jsx
+++ src/components/ui/header/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Button from "../button/Button";
 import './header.css'
 import {useTelegram} from "../../../hooks/useTelegram";
@@ -6,11 +6,27 @@ import {useTelegram} from "../../../hooks/useTelegram";
 const Header = () => {
     const {user,onClose} = useTelegram()
 
-    console.log()
+    function getWindowDimensions() {
+        const { innerWidth: width, innerHeight: height } = window;
+        return {
+            width,
+            height
+        };
+    }
+
+    const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
+    useEffect(() => {
+        function handleResize() {
+            setWindowDimensions(getWindowDimensions());
+        }
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
 
     return (
         <div className={'header'}>
             <span>{user?.username}</span>
+            <span>{windowDimensions?.width} * {windowDimensions?.height}</span>
             <Button onClick={onClose}>Закрыть</Button>
         </div>
     );
